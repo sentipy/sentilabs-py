3edb208f4051f03d7fd3ffc6bb73bc824415a83b
SentiPy <sentipy@gmail.com>
2013-12-20 14:21:30 +0400

    some minor additions

--- test/sentilabs/cs/algs/mergesort.py
+++ test/sentilabs/cs/algs/mergesort.py
@@ -4,6 +4,15 @@ from sentilabs.cs.algs.sorts import *
 
 
 class MergeSortTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        r = 1000000
+        cls.test6_arr = list()
+        for i in range(r):
+            cls.test6_arr.append(random.randint(0, 50000))
+        random.shuffle(cls.test6_arr)
+
     def test_1(self):
         arr = list(range(5))
         random.shuffle(arr)
@@ -39,6 +48,10 @@ class MergeSortTest(unittest.TestCase):
         random.shuffle(arr)
         mergeSort(arr)
         self.assertEqual(arr, sorted(arr))
+
+    def test_6(self):
+        mergeSort(self.test6_arr)
+        self.assertEqual(self.test6_arr, sorted(self.test6_arr))
 
 if __name__ == '__main__':
     unittest.main()
--- test/sentilabs/cs/algs/quick_sort_3way.py
+++ test/sentilabs/cs/algs/quick_sort_3way.py
@@ -4,6 +4,15 @@ from sentilabs.cs.algs.sorts import *
 
 
 class Quick3WaySortTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        r = 1000000
+        cls.test6_arr = list()
+        for i in range(r):
+            cls.test6_arr.append(random.randint(0, 50000))
+        random.shuffle(cls.test6_arr)
+
     def test_1(self):
         arr = list(range(5))
         random.shuffle(arr)
@@ -39,6 +48,10 @@ class Quick3WaySortTest(unittest.TestCase):
         random.shuffle(arr)
         quick_sort_3way(arr)
         self.assertEqual(arr, sorted(arr))
+
+    def test_6(self):
+        quick_sort_3way(self.test6_arr)
+        self.assertEqual(self.test6_arr, sorted(self.test6_arr))
 
 if __name__ == '__main__':
     unittest.main()
